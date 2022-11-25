de4b35bb13ebb4684733d7225973f3141ead8a41
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-11-25 03:00:35 +0100

    Refactored [id] paths to [_id]

--- pages/[_id]/index.js
+++ pages/[_id]/index.js
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import dbConnect from '../../lib/dbConnect'
-import Pet from '../../models/Pet'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import dbConnect from "../../lib/dbConnect";
+import Pet from "../../models/Pet";
+import findAndSerializeMongoDoc from "../../lib/util";
+import mongoose, { Model } from "mongoose";
 
-export async function getServerSideProps({ params }) {
-  await dbConnect()
+export async function getServerSideProps({ params: { _id } }) {
+  await dbConnect();
 
-  const pet = await Pet.findById(params.id).lean()
-  pet._id = pet._id.toString()
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return { props: { error: "Not a valid pet id" } };
+  }
 
-  return { props: { pet } }
+  const pet = await findAndSerializeMongoDoc(Pet, Model.findById, { _id });
+
+  if (!pet) {
+    return { notFound: true };
+  }
+
+  return { props: { pet } };
 }
 
 /* Allows you to view pet card info and delete pet card*/
-const PetPage = ({ pet }) => {
-  const router = useRouter()
-  const [message, setMessage] = useState('')
-  const handleDelete = async () => {
-    const petID = router.query.id
+const PetPage = ({ pet, error }) => {
+  const router = useRouter();
+  const [message, setMessage] = useState("" || error);
 
+  const handleDelete = async () => {
     try {
-      await fetch(`/api/pets/${petID}`, {
-        method: 'Delete',
-      })
-      await router.push('/')
+      await fetch(`/api/pets/${router.query._id}`, { method: "Delete" });
+      await router.push("/");
     } catch (error) {
-      setMessage('Failed to delete the pet.')
+      setMessage("Failed to delete the pet.");
     }
+  };
+
+  if (error) {
+    return <h1 className="text-xl">{error}</h1>;
   }
 
   return (
@@ -58,7 +68,7 @@ const PetPage = ({ pet }) => {
           </div>
 
           <div className="btn-container">
-            <Link href="/[id]/edit" as={`/${pet._id}/edit`}>
+            <Link href="/[_id]/edit" as={`/${pet._id}/edit`}>
               <button className="btn edit">Edit</button>
             </Link>
             <button className="btn delete" onClick={handleDelete}>
@@ -69,7 +79,7 @@ const PetPage = ({ pet }) => {
       </div>
       {message && <p>{message}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default PetPage
+export default PetPage;
