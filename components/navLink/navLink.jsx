553d2444893dbb9d7d90b1b113054bfae84ef6d6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-11-25 03:00:46 +0100

    Added navLink.jsx

--- components/navLink/navLink.jsx
+++ components/navLink/navLink.jsx
@@ -1,18 +1,17 @@
-import React from "react"
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NavLink = ({ label, href, onClick, exact }) => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const condition = exact ? router.pathname === href : router.pathname.startsWith(href)
+  const condition = exact ? router.pathname === href : router.pathname.startsWith(href);
 
   return (
-    <Link href={href} onClick={onClick}
-        className={`hover:font-bold decoration-black ${condition ? 'underline' : "no-underline"}`}>
-        {label}
+    <Link href={href} onClick={onClick} className={`hover:font-bold ${condition ? "underline" : "no-underline"}`}>
+      {label}
     </Link>
-  )
-}
+  );
+};
 
-export default NavLink
+export default NavLink;
