709c7bb7f8f9ec498428987e61d6b6f13eb516b2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-12-03 05:14:33 +0100

    Updated nav

--- components/layout/nav.jsx
+++ components/layout/nav.jsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="top-bar">
-      <div className="nav text-neutral-900 decoration-neutral-900 dark:text-neutral-100 dark:decoration-neutral-100">
-        <NavLink label="Home" href="/" exact />
-        <NavLink label="New Pet" href="/new-pet" />
-      </div>
-
+    <nav className="sticky top-0 z-10 flex h-20 w-full justify-between bg-neutral-100 px-4 text-right dark:bg-neutral-900">
       <Link href="/">
         <img
-          id="title"
           src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Pet_logo_with_flowers.png"
           alt="pet care logo"
-          className="hover:scale-110 transition-transform active:scale-100 dark:invert"
+          className="h-full transition-transform hover:scale-110 active:scale-100 dark:invert"
         />
       </Link>
-    </div>
+
+      <div className="flex items-center gap-4 text-xl dark:decoration-neutral-100">
+        <NavLink label="Home" href="/" exact />
+        <NavLink label="New Pet" href="/new-pet" />
+        <NavLink label="New Flatground Trick" href="/new-flatground-trick" />
+      </div>
+    </nav>
   );
 }
