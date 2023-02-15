d8dfbabcb4d179ee72963dd225f4b13795c746eb
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-15 23:04:58 +0100

    Updated trick form to show server side error messages, new trick format,

--- components/forms/flatgroundTrick/form.js
+++ components/forms/flatgroundTrick/form.js
@@ -1,78 +1,74 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-import { mutate } from "swr";
-import styles from "../form.module.scss";
-import { VN } from "../../../lib/util";
-import utilStyles from "../../../styles/utils.module.scss";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { mutate } from 'swr';
+import styles from '../form.module.scss';
+import { capitalize, VN } from '../../../lib/util';
+import utilStyles from '../../../styles/utils.module.scss';
+import FLATGROUND_TRICKS from '../../../models/constants/flatgroundTricks';
 
-const Form = ({ formId, flatgroundTrickForm, forNewFlatgroundTrick = true }) => {
+const headers = { Accept: 'application/json', 'Content-Type': 'application/json' };
+
+const Form = ({ flatgroundTrickForm, newFlatgroundTrick = true }) => {
   const router = useRouter();
-  const contentType = "application/json";
+
   const [errors, setErrors] = useState({});
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   const [form, setForm] = useState({
     name: flatgroundTrickForm.name,
     preferred_stance: flatgroundTrickForm.preferred_stance,
     stance: flatgroundTrickForm.stance,
     direction: flatgroundTrickForm.direction,
+    rotation: flatgroundTrickForm.rotation,
     link: flatgroundTrickForm.link,
-    description: flatgroundTrickForm.description,
     date: new Date(flatgroundTrickForm.date).toISOString().substring(0, 10),
     image_url: flatgroundTrickForm.image_url,
-    landed: !!flatgroundTrickForm.landed,
   });
 
-  const { name, preferred_stance, stance, direction, link, description, date, image_url, landed } = form;
+  const { name, preferred_stance, stance, direction, rotation, link, date, image_url } = form;
 
-  /* The PUT method edits an existing entry in the mongodb database. */
-  const putData = async (form) => {
+  /* The PATCH method edits an existing entry in the mongodb database. */
+  const patchData = async (form) => {
     const { _id } = router.query;
 
     try {
       const res = await fetch(`/api/flatgroundtricks/${_id}`, {
-        method: "PUT",
-        headers: {
-          Accept: contentType,
-          "Content-Type": contentType,
-        },
+        method: 'PATCH',
+        headers,
         body: JSON.stringify(form),
       });
 
-      // Throw error with status code in case Fetch API req failed
       if (!res.ok) {
-        throw new Error(res.status);
+        const { error } = await res.json();
+        throw new Error(error);
       }
 
       const { data } = await res.json();
 
       await mutate(`/api/flatgroundtricks/${_id}`, data, false); // Update the local data without a revalidation
-      await router.push("/");
+      await router.push('/');
     } catch (error) {
-      setMessage("Failed to update flatground trick");
+      setMessage(`Failed to update flatground trick: ${error.message}`);
     }
   };
 
   /* The POST method adds a new entry in the mongodb database. */
   const postData = async (form) => {
     try {
-      const res = await fetch("/api/flatgroundtricks", {
-        method: "POST",
-        headers: {
-          Accept: contentType,
-          "Content-Type": contentType,
-        },
+      const res = await fetch('/api/flatgroundtricks', {
+        method: 'POST',
+        headers,
         body: JSON.stringify(form),
       });
 
-      // Throw error with status code in case Fetch API req failed
       if (!res.ok) {
-        throw new Error(res.status);
+        const { error } = await res.json();
+        throw new Error(error);
       }
 
-      await router.push("/");
+      await router.push('/');
     } catch (error) {
-      setMessage("Failed to add flatgroundTrick");
+      setMessage(`Failed to add flatground trick: ${error.message}`);
     }
   };
 
@@ -80,44 +76,21 @@ const Form = ({ formId, flatgroundTrickForm, forNewFlatgroundTrick = true }) => 
     const { target } = e;
     let { value, name } = target;
 
-    if (target.type === "checkbox") {
+    if (target.type === 'checkbox') {
       value = target.checked;
     }
 
-    setForm({
-      ...form,
-      [name]: value,
-    });
+    setForm({ ...form, [name]: value });
   };
-
-  // /* Makes sure info is filled in*/
-  // const formValidate = () => {
-  //   let err = {};
-  //   if (!form.name) err.name = "Name is required";
-  //   if (!form.owner_name) err.owner_name = "Owner is required";
-  //   if (!form.species) err.species = "Species is required";
-  //   if (!form.image_url) err.image_url = "Image URL is required";
-  //   return err;
-  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // const errs = formValidate();
-    // if (Object.keys(errs).length === 0) {
-    forNewFlatgroundTrick ? postData(form) : putData(form);
-    // } else {
-    //   setErrors({ ...errs });
-    // }
+    newFlatgroundTrick ? postData(form) : patchData(form);
   };
 
   return (
     <>
-      <form id={formId} onSubmit={handleSubmit} className={styles.form}>
-        <label>
-          Name
-          <input type="text" maxLength="20" name={VN({ name })} value={name} onChange={handleChange} required />
-        </label>
-
+      <form onSubmit={handleSubmit} className={styles.form}>
         <label>
           Preferred stance
           <select name={VN({ preferred_stance })} value={preferred_stance} onChange={handleChange} required>
@@ -126,51 +99,66 @@ const Form = ({ formId, flatgroundTrickForm, forNewFlatgroundTrick = true }) => 
           </select>
         </label>
 
-        <label>
-          Stance
-          <select name={VN({ stance })} value={stance} onChange={handleChange} required>
-            <option value="regular">Regular</option>
-            <option value="fakie">Fakie</option>
-            <option value="switch">Switch</option>
-            <option value="nollie">Nollie</option>
-          </select>
-        </label>
+        <div className="flex justify-between">
+          <label>
+            Stance
+            <select name={VN({ stance })} value={stance} onChange={handleChange} required>
+              <option value="regular">-</option>
+              <option value="fakie">Fakie</option>
+              <option value="switch">Switch</option>
+              <option value="nollie">Nollie</option>
+            </select>
+          </label>
 
-        <label>
-          Direction
-          <select name={VN({ direction })} value={direction} onChange={handleChange}>
-            <option selected value="none">
-              None
-            </option>
-            <option value="frontside">Frontside</option>
-            <option value="backside">Backside</option>
-          </select>
-        </label>
+          <label>
+            Direction
+            <select name={VN({ direction })} value={direction} onChange={handleChange}>
+              <option selected value="none">
+                -
+              </option>
+              <option value="frontside">Frontside</option>
+              <option value="backside">Backside</option>
+            </select>
+          </label>
 
-        <label>
-          Description
-          <textarea name={VN({ description })} value={description} onChange={handleChange} />
-        </label>
+          <label>
+            Rotation
+            <select name={VN({ rotation })} value={rotation} onChange={handleChange} required>
+              <option value="0">-</option>
+              <option value="180">180</option>
+              <option value="360">360</option>
+              <option value="540">540</option>
+              <option value="720">720</option>
+            </select>
+          </label>
 
-        <label>
-          Date
-          <input type="date" name={VN({ date })} value={date} onChange={handleChange} />
-        </label>
+          <label>
+            Name
+            <select name={VN({ name })} value={name} onChange={handleChange} required>
+              {FLATGROUND_TRICKS.map((trick) => (
+                <option key={trick} value={trick}>
+                  {capitalize(trick)}
+                </option>
+              ))}
+              <option value="other">Other</option>
+            </select>
+          </label>
+        </div>
 
-        <label>
-          Landed
-          <input type="checkbox" name={VN({ landed })} checked={!!landed} onChange={handleChange} />
-        </label>
+        {/*<label>*/}
+        {/*  Date*/}
+        {/*  <input type="date" name={VN({date})} value={date} onChange={handleChange} />*/}
+        {/*</label>*/}
 
         <label>
           Image URL
           <input type="url" name={VN({ image_url })} value={image_url} onChange={handleChange} />
         </label>
 
-        <label>
-          Link
-          <input type="url" name={VN({ link })} value={link} onChange={handleChange} required />
-        </label>
+        {/*<label>*/}
+        {/*  Link*/}
+        {/*  <input type="url" name={VN({link})} value={link} onChange={handleChange} required />*/}
+        {/*</label>*/}
 
         <p className="my-4">{message}</p>
 
