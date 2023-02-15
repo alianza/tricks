529d786b3ef716cbf981f51a2c44476c253f0c2b
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-15 23:06:30 +0100

    Updated flatgroundTrick mathods to return error messages

--- pages/api/flatgroundtricks/index.js
+++ pages/api/flatgroundtricks/index.js
@@ -1,5 +1,5 @@
-import dbConnect from "../../../lib/dbConnect";
-import FlatgroundTrick from "../../../models/FlatgroundTrick";
+import dbConnect from '../../../lib/dbConnect';
+import FlatgroundTrick from '../../../models/FlatgroundTrick';
 
 export default async function handler(req, res) {
   const { method } = req;
@@ -9,23 +9,23 @@ export default async function handler(req, res) {
   await dbConnect();
 
   switch (method) {
-    case "GET":
+    case 'GET':
       try {
         const flatgroundTrick = await FlatgroundTrick.find({}); /* find all the data in our database */
         res.status(200).json({ success: true, data: flatgroundTrick });
       } catch (error) {
         console.error(error);
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, error: error.message });
       }
       break;
-    case "POST":
+    case 'POST':
       try {
         const flatgroundTrick = await FlatgroundTrick.create(req.body); /* create a new model in the database */
         console.log(`flatgroundTrick`, flatgroundTrick);
         res.status(201).json({ success: true, data: flatgroundTrick });
       } catch (error) {
         console.error(error);
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, error: error.message });
       }
       break;
     default:
