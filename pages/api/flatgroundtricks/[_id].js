277eba0371db20f29b692f2ad27e398629d37832
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-15 23:06:08 +0100

    Updated flatgroundtrick methods to return error messages

--- pages/api/flatgroundtricks/[_id].js
+++ pages/api/flatgroundtricks/[_id].js
@@ -1,5 +1,5 @@
-import dbConnect from "../../../lib/dbConnect";
-import FlatGroundTrick from "../../../models/FlatgroundTrick";
+import dbConnect from '../../../lib/dbConnect';
+import FlatGroundTrick from '../../../models/FlatgroundTrick';
 
 export default async function handler(req, res) {
   const {
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   await dbConnect();
 
   switch (method) {
-    case "GET" /* Get a model by its ID */:
+    case 'GET':
       try {
         const flatgroundTrick = await FlatGroundTrick.findById(_id);
         if (!flatgroundTrick) {
@@ -19,11 +19,11 @@ export default async function handler(req, res) {
         res.status(200).json({ success: true, data: flatgroundTrick });
       } catch (error) {
         console.error(error);
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, error: error.message });
       }
       break;
 
-    case "PUT" /* Edit a model by its ID */:
+    case 'PATCH':
       try {
         const flatgroundTrick = await FlatGroundTrick.findByIdAndUpdate(_id, req.body, {
           new: true,
@@ -35,11 +35,11 @@ export default async function handler(req, res) {
         res.status(200).json({ success: true, data: flatgroundTrick });
       } catch (error) {
         console.error(error);
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, error: error.message });
       }
       break;
 
-    case "DELETE" /* Delete a model by its ID */:
+    case 'DELETE':
       try {
         const deletedTrick = await FlatGroundTrick.deleteOne({ _id });
         if (!deletedTrick) {
@@ -48,13 +48,14 @@ export default async function handler(req, res) {
         res.status(200).json({ success: true, data: {} });
       } catch (error) {
         console.error(error);
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, error: error.message });
       }
       break;
 
     default:
-      console.error("Unhandled method: ", method);
-      res.status(400).json({ success: false });
+      const errorMessage = `Unhandled method: ${method}`;
+      console.error(errorMessage);
+      res.status(400).json({ success: false, error: errorMessage });
       break;
   }
 }
