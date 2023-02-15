04b3adf73466250f61861400e738b28321ad87f0
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-15 23:08:03 +0100

    Added progress bar to _app.js lazy loaded

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import "../styles/globals.scss";
-import Layout from "../components/layout/layout";
+import '../styles/globals.scss';
+import Layout from '../components/layout/layout';
+import dynamic from 'next/dynamic';
+const NextNProgress = dynamic(() => import('nextjs-progressbar'), { loading: () => <div /> });
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <NextNProgress />
     </Layout>
   );
 }
