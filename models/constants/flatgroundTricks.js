6cab216422c486272939dfba89d62d840156222c
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-15 23:05:28 +0100

    Added flatGroundTricks constant

---
+++ models/constants/flatgroundTricks.js
@@ -0,0 +1,17 @@
+const FLATGROUND_TRICKS = [
+  'ollie',
+  'shove-it',
+  'kickflip',
+  'heelflip',
+  'varial kickflip',
+  'varial heelflip',
+  'nollie kickflip',
+  'nollie heelflip',
+  'nollie varial kickflip',
+  'nollie varial heelflip',
+  'hardflip',
+  'inward heelflip',
+  'inward kickflip',
+];
+
+export default FLATGROUND_TRICKS;
