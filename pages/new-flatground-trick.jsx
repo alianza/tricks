bb33806ab4c897d2d5ec937f41c98f788c6b9d96
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-15 23:09:19 +0100

    Rename jsx components with .jsx file extension

---
+++ pages/new-flatground-trick.jsx
@@ -0,0 +1,19 @@
+import Form from '../components/forms/flatgroundTrick/form';
+import FLATGROUND_TRICKS from '../models/constants/flatgroundTricks';
+
+const NewFlatFlatGroundTrick = () => {
+  const flatgroundTrickForm = {
+    name: FLATGROUND_TRICKS[0],
+    preferred_stance: 'regular',
+    stance: 'regular',
+    direction: 'none',
+    rotation: 0,
+    date: new Date().toISOString().substring(0, 10),
+    link: '',
+    image_url: '',
+  };
+
+  return <Form flatgroundTrickForm={flatgroundTrickForm} />;
+};
+
+export default NewFlatFlatGroundTrick;
