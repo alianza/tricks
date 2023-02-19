ba3c6bcaae0d8efff348a285987c46df48c6978a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-19 22:12:32 +0100

    Added grinds list

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,40 @@
-import dbConnect from "../lib/dbConnect";
-import findAndSerializeDoc from "../lib/util";
-import FlatGroundTrick from "../models/FlatgroundTrick";
-import { Model } from "mongoose";
-import FlatgroundTrickCard from "../components/cards/flatgroundTrick/flatgroundTrickCard";
+import dbConnect from '../lib/dbConnect';
+import findAndSerializeDoc from '../lib/util';
+import FlatGroundTrick from '../models/FlatgroundTrick';
+import Grind from '../models/Grind';
+import { Model } from 'mongoose';
+import FlatgroundTrickCard from '../components/cards/flatgroundTrickCard';
+import GrindCard from '../components/cards/grindCard';
 
 export async function getServerSideProps() {
   await dbConnect();
 
   const flatgroundTricks = await findAndSerializeDoc(FlatGroundTrick, Model.find, {});
 
-  return { props: { flatgroundTricks } };
+  const grinds = await findAndSerializeDoc(Grind, Model.find, {});
+
+  return { props: { flatgroundTricks, grinds } };
 }
 
-const Index = ({ flatgroundTricks }) => {
+const Index = ({ flatgroundTricks, grinds }) => {
   return (
-    <div className="flex flex-col items-center gap-16">
-      <h1 className="-mb-12 text-4xl">Flatground Tricks</h1>
-      <div id="flat-ground-tricks" className="flex flex-wrap gap-2">
-        {flatgroundTricks.map((flatgroundTrick) => (
-          <FlatgroundTrickCard
-            key={flatgroundTrick._id}
-            flatgroundTrick={flatgroundTrick}
-            mode="view"
-          />
-        ))}
+    <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center">
+        <h1 className="mb-6 text-4xl">Flatground Tricks</h1>
+        <div className="flex flex-wrap justify-center gap-4">
+          {flatgroundTricks.map((flatgroundTrick) => (
+            <FlatgroundTrickCard key={flatgroundTrick._id} flatgroundTrick={flatgroundTrick} mode="view" />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="mb-6 text-4xl">Grinds</h1>
+        <div className="flex flex-wrap justify-center gap-4">
+          {grinds.map((grind) => (
+            <GrindCard key={grind._id} grind={grind} mode="view" />
+          ))}
+        </div>
       </div>
     </div>
   );
