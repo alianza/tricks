e3867769dbf9078c4e3bb5ff5ff305b40dd21a1d
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-20 01:51:29 +0100

    Updated trick enums

--- models/constants/flatgroundTricks.js
+++ models/constants/flatgroundTricks.js
@@ -5,10 +5,6 @@ const FLATGROUND_TRICKS = {
   heelflip: 'heelflip',
   varial_kickflip: 'varial kickflip',
   varial_heelflip: 'varial heelflip',
-  nollie_kickflip: 'nollie kickflip',
-  nollie_heelflip: 'nollie heelflip',
-  nollie_varial_kickflip: 'nollie varial kickflip',
-  nollie_varial_heelflip: 'nollie varial heelflip',
   hardflip: 'hardflip',
   inward_heelflip: 'inward heelflip',
 };
