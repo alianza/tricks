928a055e681bf2a1ad17fd93e55fdd9db5f1a3c0
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-26 18:04:15 +0100

    Added table.jsx and tableDataRow.jsx components

---
+++ components/common/table.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import { capitalize } from '../../lib/util';
+import { ChevronDownIcon, ChevronUpDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid';
+import TableDataRow from './tableDataRow';
+
+const Table = ({ objArray, columns, actions, endpoint }) => {
+  const [columnSortDirectionMap, setColumnSortDirectionMap] = useState({});
+  const [objArrayState, setObjArrayState] = useState(objArray);
+  const [message, setMessage] = useState(null);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  columns = actions.length ? [...columns, 'actions'] : columns;
+
+  useEffect(() => {
+    sort(columns[0], 'asc');
+    if (!objArrayState.length) setIsEmpty(true);
+  }, [objArrayState]);
+
+  const sort = (column, direction) => {
+    setObjArrayState(
+      objArrayState.sort((a, b) => {
+        if (a[column] > b[column]) return direction === 'asc' ? 1 : -1;
+        if (a[column] < b[column]) return direction === 'asc' ? -1 : 1;
+        return 0;
+      })
+    );
+    setColumnSortDirectionMap({ [column]: direction });
+  };
+
+  const deleteRow = (id) => setObjArrayState(objArrayState.filter((obj) => obj._id !== id));
+
+  return (
+    <>
+      <table className="mx-auto table-auto">
+        <thead className="bg-neutral-200 dark:bg-neutral-700">
+          <tr>
+            {columns.map((column) => (
+              <th key={column.toString()} className="p-4">
+                <div className={`flex justify-center gap-2`}>
+                  <p>{capitalize(column)}</p>
+                  {columnSortDirectionMap[column] === 'asc' && (
+                    <ChevronDownIcon
+                      onClick={() => sort(column, 'desc')}
+                      className="h-6 w-6 cursor-pointer"
+                    ></ChevronDownIcon>
+                  )}
+                  {columnSortDirectionMap[column] === 'desc' && (
+                    <ChevronUpIcon
+                      onClick={() => sort(column, 'asc')}
+                      className="h-6 w-6 cursor-pointer"
+                    ></ChevronUpIcon>
+                  )}
+                  {column !== 'actions' && !columnSortDirectionMap[column] && (
+                    <ChevronUpDownIcon
+                      onClick={() => sort(column, 'asc')}
+                      className="h-6 w-6 cursor-pointer"
+                    ></ChevronUpDownIcon>
+                  )}
+                </div>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-neutral-50 dark:bg-neutral-800">
+          {isEmpty && (
+            <tr>
+              <td className="p-4 text-center" colSpan={columns.length}>{`No ${endpoint} yet...`}</td>
+            </tr>
+          )}
+          {objArrayState.map((obj) => (
+            <TableDataRow
+              key={obj._id}
+              obj={obj}
+              columns={columns}
+              actions={actions}
+              endpoint={endpoint}
+              emitMessage={(message) => setMessage(message)}
+              deleteRow={deleteRow}
+            />
+          ))}
+        </tbody>
+      </table>
+      {message && <p className="mx-auto my-2 font-bold text-red-500">{message}</p>}
+    </>
+  );
+};
+
+export default Table;
---
+++ components/common/tableDataRow.jsx
@@ -0,0 +1,95 @@
+import { capitalize, getFullName } from '../../lib/util';
+import Link from 'next/link';
+import utilStyles from '../../styles/utils.module.scss';
+
+const tableDataRow = ({ obj, columns, actions, endpoint, emitMessage, deleteRow }) => {
+  const objColumnMap = {};
+
+  columns.forEach((column) => {
+    if (column === 'actions') {
+      objColumnMap[column] = actions;
+    } else {
+      objColumnMap[column] = obj[column];
+    }
+  });
+
+  const handleDelete = async () => {
+    if (!confirm(`Are you sure you want to delete "${getFullName(obj, endpoint)}"?`)) return;
+
+    try {
+      const response = await fetch(`/api/${endpoint}/${obj._id}`, { method: 'Delete' });
+      if (!response.ok) throw new Error(`Failed to delete ${getFullName(obj, endpoint)}`);
+      deleteRow(obj._id);
+      emitMessage(null);
+    } catch (error) {
+      emitMessage(`Failed to delete ${getFullName(obj, endpoint)}`);
+    }
+  };
+
+  return (
+    <tr className="border-t-2 border-neutral-400">
+      {Object.entries(objColumnMap).map(([key, value]) => {
+        if (key === 'actions') {
+          const colOutput = value.map((action) => {
+            switch (action) {
+              case 'edit':
+                return <EditButton endpoint={endpoint} id={obj._id} />;
+              case 'view':
+                return <ViewButton endpoint={endpoint} id={obj._id} />;
+              case 'delete':
+                return <DeleteButton handleDelete={handleDelete} />;
+              default:
+                return null;
+            }
+          });
+
+          return (
+            <td key={key} className="p-4">
+              <div className="flex justify-center gap-2">{colOutput}</div>
+            </td>
+          );
+        }
+
+        if (key === 'trick') {
+          return (
+            <td key={key} className="p-4 text-sm">
+              {value}
+            </td>
+          );
+        }
+
+        return (
+          <td key={key} className="p-4">
+            {capitalize(value)}
+          </td>
+        );
+      })}
+    </tr>
+  );
+};
+
+function EditButton({ endpoint, id }) {
+  return (
+    <Link href={`/${endpoint}/[_id]/edit`} as={`/${endpoint}/${id}/edit`}>
+      <button className={`${utilStyles.button} bg-green-500 focus:ring-green-600/50 hover:bg-green-600`}>Edit</button>
+    </Link>
+  );
+}
+
+function ViewButton({ endpoint, id }) {
+  return (
+    <Link href={`/${endpoint}/[_id]`} as={`/${endpoint}/${id}`}>
+      <button className={`${utilStyles.button} bg-blue-500 focus:ring-blue-600/50 hover:bg-blue-600`}>View</button>
+    </Link>
+  );
+}
+
+function DeleteButton({ handleDelete }) {
+  return (
+    <div onClick={handleDelete}>
+      <button className={`${utilStyles.button} bg-red-500 focus:ring-red-600/50 hover:bg-red-600`}>Delete</button>
+    </div>
+  );
+}
+
+export default tableDataRow;
