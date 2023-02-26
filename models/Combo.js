19473d72e5e4ca176be1b8d4c3ddf633d752ddc6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-26 23:51:09 +0100

    Added combo model

---
+++ models/Combo.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import { GRINDS_ENUM } from './constants/grinds';
+import FlatgroundTrick from './FlatgroundTrick';
+import Grind from './Grind';
+
+const TRICK_TYPES_ENUM = [FlatgroundTrick.collection.collectionName, Grind.collection.collectionName];
+
+const ComboSchema = new mongoose.Schema(
+  {
+    trickArray: [
+      {
+        _id: {
+          type: mongoose.Schema.Types.ObjectId,
+          refPath: 'trickRef',
+        },
+        trickRef: {
+          type: String,
+          enum: TRICK_TYPES_ENUM,
+          default: TRICK_TYPES_ENUM[0],
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// trickArray should have at least a length of 2
+async function validation(next, self) {
+  if (self.trickArray.length < 2) {
+    next(new Error('Combo must consist of at least 2 tricks'));
+  }
+  next();
+}
+
+ComboSchema.pre('validate', async function (next) {
+  await validation(next, this);
+});
+ComboSchema.pre('findOneAndUpdate', async function (next) {
+  await validation(next, this.getUpdate());
+});
+
+export default mongoose.models.Combo || mongoose.model('Combo', ComboSchema);
