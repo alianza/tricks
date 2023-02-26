cfe8c8b92dd65cedc4d1dba16e08eec7ea3b67cf
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-02-26 23:54:03 +0100

    Added new combo page

---
+++ pages/new-combo.jsx
@@ -0,0 +1,11 @@
+import ComboForm from '../components/forms/comboForm';
+
+const newCombo = () => {
+  const comboForm = {
+    trickArray: [],
+  };
+
+  return <ComboForm comboForm={comboForm} />;
+};
+
+export default newCombo;
