e55ac5d1d667a47b809415f34074a4acae010371
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-01 20:05:13 +0100

    Added destructured props to findAndSerializeDoc

--- pages/flatgroundtricks/[_id]/index.jsx
+++ pages/flatgroundtricks/[_id]/index.jsx
@@ -11,7 +11,11 @@ export async function getServerSideProps({ params: { _id } }) {
     return { props: { error: `${_id} is not a valid flatground trick id...` } };
   }
 
-  const flatgroundTrick = await findAndSerializeDoc(FlatGroundTrick, Model.findById, { _id });
+  const flatgroundTrick = await findAndSerializeDoc({
+    model: FlatGroundTrick,
+    operation: Model.findById,
+    query: { _id },
+  });
 
   if (!flatgroundTrick) {
     return { notFound: true };
