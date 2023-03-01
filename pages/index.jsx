19385c768744526b2728ca3b07c3421b0f260d7a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-01 16:52:32 +0100

    Added combos to homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
 import dbConnect from '../lib/dbConnect';
-import { getTricks } from '../lib/util';
+import findAndSerializeDoc, { getTricks } from '../lib/util';
 import FlatGroundTrick from '../models/FlatgroundTrick';
 import Grind from '../models/Grind';
 import { Model } from 'mongoose';
 import Table from '../components/common/table/table';
+import Combo from '../models/Combo';
 
 export async function getServerSideProps() {
   await dbConnect();
@@ -16,12 +17,38 @@ export async function getServerSideProps() {
   const grindColumns = ['stance', 'direction', 'name', 'trick'];
   const grindActions = ['edit', 'view', 'delete'];
 
+  let combos = await findAndSerializeDoc(Combo, Model.find);
+  const comboColumns = ['trickArray'];
+  const comboActions = ['edit', 'view', 'delete'];
+
+  console.log(`combos`, combos);
+
   return {
-    props: { flatgroundTricks, flatgroundColumns, flatgroundActions, grinds, grindColumns, grindActions },
+    props: {
+      flatgroundTricks,
+      flatgroundColumns,
+      flatgroundActions,
+      grinds,
+      grindColumns,
+      grindActions,
+      combos,
+      comboColumns,
+      comboActions,
+    },
   };
 }
 
-const Index = ({ flatgroundTricks, flatgroundColumns, flatgroundActions, grinds, grindColumns, grindActions }) => {
+const Index = ({
+  flatgroundTricks,
+  flatgroundColumns,
+  flatgroundActions,
+  grinds,
+  grindColumns,
+  grindActions,
+  combos,
+  comboColumns,
+  comboActions,
+}) => {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col">
@@ -40,6 +67,13 @@ const Index = ({ flatgroundTricks, flatgroundColumns, flatgroundActions, grinds,
         <h1 className="mx-auto mb-6 text-4xl">Grinds</h1>
         <div className="overflow-x-auto">
           <Table objArray={grinds} columns={grindColumns} actions={grindActions} endpoint="grinds" />
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <h1 className="mx-auto mb-6 text-4xl">Combos</h1>
+        <div className="overflow-x-auto">
+          <Table objArray={combos} columns={comboColumns} actions={comboActions} endpoint="combos" />
         </div>
       </div>
     </div>
