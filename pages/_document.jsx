7bad25ae429a034b80b15dc28f0043aa60df23f2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-03 14:32:41 +0100

    Added lang

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html className="scrollbar-thin scrollbar-thumb-neutral-500">
+    <Html lang="en">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
