9d24036c354629aea2b3ba21ac6c12319618aac2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-03 14:32:00 +0100

    Remove tailwind scrollbar plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [],
 };
