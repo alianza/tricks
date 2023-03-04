ce61b863e19f36e299c327cccb30fd8f3179634a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-04 03:16:56 +0100

    Fix endpoint

--- pages/api/combos/[_id].js
+++ pages/api/combos/[_id].js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   switch (method) {
     case 'GET':
       try {
-        const combo = await Combo.findById(_id).populate('trickArray.ref');
+        const combo = await Combo.findById(_id).populate('trickArray.trick');
         if (!combo) {
           return res.status(400).json({ success: false, error: `Combo with id "${_id}" not found.` });
         }
