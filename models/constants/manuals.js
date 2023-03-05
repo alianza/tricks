c22ef33fa59b781492ea01f72547f41005c2db0b
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-05 20:53:09 +0100

    Added manual constants

---
+++ models/constants/manuals.js
@@ -0,0 +1,14 @@
+const MANUALS = {
+  regular: 'regular-manual',
+  nose: 'nose-manual',
+  switch: 'switch-manual',
+  fakie: 'fakie-manual',
+};
+
+const MANUALS_ENUM = Object.values(MANUALS);
+
+const DEFAULT_MANUAL = MANUALS.regular;
+
+export { MANUALS_ENUM, MANUALS, DEFAULT_MANUAL };
+
+export default MANUALS;
