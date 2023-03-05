ecc47fee5a04b973c93cd6364c1e03c4a9736066
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-05 21:08:22 +0100

    Added manuals to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import Grind from '../models/Grind';
 import { Model } from 'mongoose';
 import Table from '../components/common/table/table';
 import Combo from '../models/Combo';
+import Manual from '../models/Manual';
 
 export async function getServerSideProps() {
   await dbConnect();
@@ -17,65 +18,84 @@ export async function getServerSideProps() {
   const grindColumns = ['stance', 'direction', 'name', 'trick'];
   const grindActions = ['edit', 'view', 'delete'];
 
+  const manuals = await findAndSerializeDoc({ model: Manual, operation: Model.find });
+  const manualColumns = ['type'];
+  const manualActions = ['edit', 'view', 'delete'];
+
   let combos = await findAndSerializeDoc({ model: Combo, operation: Model.find, populateFields: ['trickArray.trick'] });
   combos = combos.map(populateComboTrickName);
   combos = combos.map(({ _id, trickArray }) => ({ _id, combo: getFullComboName({ trickArray }) }));
-
   const comboColumns = ['combo'];
   const comboActions = ['edit', 'view', 'delete'];
 
   return {
     props: {
-      flatgroundTricks,
-      flatgroundColumns,
-      flatgroundActions,
-      grinds,
-      grindColumns,
-      grindActions,
-      combos,
-      comboColumns,
-      comboActions,
+      flatgroundTricks: {
+        flatgroundTricks,
+        flatgroundColumns,
+        flatgroundActions,
+      },
+      grinds: {
+        grinds,
+        grindColumns,
+        grindActions,
+      },
+      combos: {
+        combos,
+        comboColumns,
+        comboActions,
+      },
+      manuals: {
+        manuals,
+        manualColumns,
+        manualActions,
+      },
     },
   };
 }
 
-const Index = ({
-  flatgroundTricks,
-  flatgroundColumns,
-  flatgroundActions,
-  grinds,
-  grindColumns,
-  grindActions,
-  combos,
-  comboColumns,
-  comboActions,
-}) => {
+const Index = ({ flatgroundTricks, grinds, combos, manuals }) => {
   return (
     <div className="flex flex-col gap-16">
       <div className="flex flex-col">
         <h1 className="mx-auto mb-6 text-4xl">Flatground Tricks</h1>
         <Table
-          objArray={flatgroundTricks}
-          columns={flatgroundColumns}
-          actions={flatgroundActions}
+          objArray={flatgroundTricks.flatgroundTricks}
+          columns={flatgroundTricks.flatgroundColumns}
+          actions={flatgroundTricks.flatgroundActions}
           endpoint="flatgroundtricks"
         />
       </div>
 
       <div className="flex flex-col">
         <h1 className="mx-auto mb-6 text-4xl">Grinds</h1>
-        <Table objArray={grinds} columns={grindColumns} actions={grindActions} endpoint="grinds" />
+        <Table
+          objArray={grinds.grinds}
+          columns={grinds.grindColumns}
+          actions={grinds.grindActions}
+          endpoint="grinds"
+          // grindTable
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <h1 className="mx-auto mb-6 text-4xl">Manuals</h1>
+        <Table
+          objArray={manuals.manuals}
+          columns={manuals.manualColumns}
+          actions={manuals.manualActions}
+          endpoint="manuals"
+        />
       </div>
 
       <div className="flex flex-col">
         <h1 className="mx-auto mb-6 text-4xl">Combos</h1>
         <Table
-          objArray={combos}
-          columns={comboColumns}
-          actions={comboActions}
+          objArray={combos.combos}
+          columns={combos.comboColumns}
+          actions={combos.comboActions}
           endpoint="combos"
           updateLocalState
-          // comboTable
         />
       </div>
     </div>
