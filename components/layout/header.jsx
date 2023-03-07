609405a0bcdf82ea20be9103b635d463a5453431
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-07 19:15:17 +0100

    Fix header

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -18,16 +18,16 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-10 flex h-20 w-full items-center justify-between bg-blue-600 p-4 text-right shadow-xl">
-      <Link href="/" className="h-full grow-0">
+      <Link href="/" className="h-[120%] shrink-0">
         <img
           src="/logo.webp"
           alt="Trick logo"
-          className="h-full w-full transition-transform hover:scale-110 active:scale-100"
+          className="white h-full w-full transition-transform hover:scale-110 active:scale-100"
         />
       </Link>
 
       <nav
-        className={`invisible absolute top-0 bottom-0 left-0 flex h-screen w-full flex-col items-center justify-center gap-4 bg-neutral-900/80 text-xl text-neutral-50 opacity-0 transition-[opacity,visibility] duration-500 dark:decoration-neutral-100 sm:visible sm:static sm:h-auto sm:w-auto sm:flex-row sm:bg-transparent sm:text-inherit sm:opacity-100`}
+        className={`invisible absolute top-0 bottom-0 left-0 flex h-screen w-full flex-col flex-wrap items-center justify-center gap-4 whitespace-nowrap bg-neutral-900/80 text-3xl text-neutral-50 opacity-0 transition-[opacity,visibility] duration-500 dark:decoration-neutral-100 sm:visible sm:static sm:h-auto sm:w-auto sm:flex-row sm:bg-transparent sm:text-xl sm:text-inherit sm:opacity-100`}
         style={open ? openMobileMenuStyle : {}}
         onClick={() => setOpen(false)}
       >
