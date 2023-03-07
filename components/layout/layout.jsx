a9ffecd785094fc7a518e0611f5eb02c6ab8a796
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-07 19:07:15 +0100

    Added meta tags for new icons

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -12,8 +12,19 @@ export default function Layout({ children }) {
         <title>Skateboarding Trick List - Jan-Willem van Bremen</title>
         <meta
           name="description"
-          content="A modern web application to catalog skate tricks, grind, manuals and combinations of those - made by Jan-Willem van Bremen"
+          content="A modern web application to catalog skate tricks, grind, manuals and combinations - made by Jan-Willem van Bremen"
         />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=yes, viewport-fit=cover"
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#2563eb" />
       </Head>
 
       <Header />
