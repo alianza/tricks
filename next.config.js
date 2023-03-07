3b3d1c484f14d7bbcf20f969848ae3757260bd8f
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-07 19:08:18 +0100

    Added next-pwa config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true' });
+const prod = process.env.NODE_ENV === 'production';
 
-module.exports = withBundleAnalyzer({});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: !prod,
+    // buildExcludes: [/middleware-manifest\.json$/]
+});
+
+const nextConfig = {
+
+};
+
+
+module.exports = withBundleAnalyzer(
+    withPWA({
+        ...nextConfig,
+    })
+);
