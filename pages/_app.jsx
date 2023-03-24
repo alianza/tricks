6218bfdf679b10631938b741c6730ba9944d9180
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-24 01:15:42 +0100

    Added sessionProvider next-auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,13 +3,17 @@ import Layout from '../components/layout/layout';
 import dynamic from 'next/dynamic';
 const NextNProgress = dynamic(() => import('nextjs-progressbar'), { loading: () => <div /> });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <NextNProgress color="white" />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+        <NextNProgress color="white" />
+      </Layout>
+    </SessionProvider>
   );
 }
+
+import { SessionProvider } from 'next-auth/react';
 
 export default MyApp;
