8f4e77c0ca0c2d09e1bfd7f4052031aaebb346ad
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-28 22:07:52 +0200

    Added comments

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,10 +11,12 @@ export const authOptions = {
   ],
 
   callbacks: {
+    // attach user id to session
     async session({ session, user, token }) {
       session.user.id = token.id;
       return session;
     },
+    // attach user id to token
     async jwt({ token, account, user, profile }) {
       user && (token.id = user.id);
       account && (token.id = profile.id);
