2fe0bbb3ebb3606ad0d1f1d90a8aa0764d152cbf
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-03-28 22:07:26 +0200

    Nav is always mobile style

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -23,28 +23,33 @@ export default function Header() {
       <Link href="/" className="h-[120%] shrink-0">
         <img
           src="/logo.webp"
-          alt="Trick logo"
+          alt="Skateboard Trick Tracker Logo"
           className="white h-full w-full transition-transform hover:scale-110 active:scale-100"
         />
       </Link>
 
       <nav
-        className={`invisible absolute top-0 bottom-0 left-0 flex h-screen w-full flex-col flex-wrap items-center justify-center gap-4 whitespace-nowrap bg-neutral-900/80 text-3xl opacity-0 transition-[opacity,visibility] duration-500 dark:decoration-neutral-100 sm:visible sm:static sm:h-auto sm:w-auto sm:flex-row sm:bg-transparent sm:text-xl sm:opacity-100`}
+        className={`invisible absolute top-0 bottom-0 left-0 flex h-screen w-full flex-col flex-wrap items-center justify-center gap-4 whitespace-nowrap bg-neutral-900/80 text-3xl opacity-0 transition-[opacity,visibility] duration-500 dark:decoration-neutral-100`}
         style={open ? openMobileMenuStyle : {}}
         onClick={() => setOpen(false)}
       >
         <NavLink label="Home" href="/" exact />
+        <NavLink label="Dashboard" href="/dashboard" />
         <NavLink label="New Flatground Trick" href="/new-flatground-trick" />
         <NavLink label="New Grind" href="/new-grind" />
         <NavLink label="New Manual" href="/new-manual" />
         <NavLink label="New Combo" href="/new-combo" />
-        {session && <button onClick={() => signOut()}>Sign out</button>}
+        {session && (
+          <a className="cursor-pointer hover:font-bold" onClick={() => signOut()}>
+            Sign out
+          </a>
+        )}
       </nav>
 
       {open ? (
-        <XMarkIcon onClick={() => setOpen(false)} className="z-10 h-8 w-8 cursor-pointer sm:block " />
+        <XMarkIcon onClick={() => setOpen(false)} className="z-10 h-8 w-8 cursor-pointer" />
       ) : (
-        <Bars3Icon onClick={() => setOpen(true)} className="z-10 h-8 w-8 cursor-pointer sm:hidden" />
+        <Bars3Icon onClick={() => setOpen(true)} className="z-10 h-8 w-8 cursor-pointer" />
       )}
     </header>
   );
