e74dfa5281c3f20792b6449e77d183018d2eca65
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-01 18:38:17 +0200

    Added login status to header

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -2,7 +2,8 @@ import NavLink from '../common/navLink';
 import Link from 'next/link';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid';
 import { useEffect, useState } from 'react';
-import { signOut, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Header() {
   const { data: session } = useSession();
@@ -28,6 +29,27 @@ export default function Header() {
         />
       </Link>
 
+      <div className="ml-auto mr-4 flex items-center gap-2">
+        {session ? (
+          <>
+            <span>
+              Signed in as: <b>{session.user.name}</b>
+            </span>
+            <Image
+              src={session.user.image}
+              className="rounded-full"
+              alt="User profile picture"
+              width={32}
+              height={32}
+            />
+          </>
+        ) : (
+          <a className="cursor-pointer text-lg hover:font-bold" onClick={() => signIn()}>
+            Sign in
+          </a>
+        )}
+      </div>
+
       <nav
         className={`invisible absolute top-0 bottom-0 left-0 flex h-screen w-full flex-col flex-wrap items-center justify-center gap-4 whitespace-nowrap bg-neutral-900/80 text-3xl opacity-0 transition-[opacity,visibility] duration-500 dark:decoration-neutral-100`}
         style={open ? openMobileMenuStyle : {}}
@@ -40,7 +62,7 @@ export default function Header() {
         <NavLink label="New Manual" href="/new-manual" />
         <NavLink label="New Combo" href="/new-combo" />
         {session && (
-          <a className="cursor-pointer hover:font-bold" onClick={() => signOut()}>
+          <a className="cursor-pointer hover:font-bold" onClick={() => signOut({ callbackUrl: '/' })}>
             Sign out
           </a>
         )}
