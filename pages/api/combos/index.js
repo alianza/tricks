471015aec7911ef524064c15e0fb4833cb9cf327
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-01 18:42:06 +0200

    Added authorization to Api routes

--- pages/api/combos/index.js
+++ pages/api/combos/index.js
@@ -1,16 +1,26 @@
 import dbConnect from '../../../lib/dbConnect';
 import Combo from '../../../models/Combo';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+import { populateComboName, populateComboTrickName } from '../../../lib/util';
 
 export default async function handler(req, res) {
   const { method } = req;
 
   await dbConnect();
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) return res.status(401).json({ success: false, error: 'Unauthorized' });
+  const authQuery = { userId: session.user.id };
 
   switch (method) {
     case 'GET':
       try {
-        const combo = await Combo.find({}).populate('trickArray.trick').lean();
-        res.status(200).json({ success: true, data: combo });
+        let combos = await Combo.find({ ...authQuery })
+          .populate('trickArray.trick')
+          .lean();
+        combos = combos.map(populateComboTrickName); // Populate every trick name in the combo
+        combos = combos.map(populateComboName); // Populate every combo name
+        res.status(200).json({ success: true, data: combos });
       } catch (error) {
         console.error(error);
         res.status(400).json({ success: false, error: error.message });
@@ -18,7 +28,7 @@ export default async function handler(req, res) {
       break;
     case 'POST':
       try {
-        const combo = await Combo.create(req.body);
+        const combo = await Combo.create({ ...req.body, ...authQuery });
         res.status(201).json({ success: true, data: combo });
       } catch (error) {
         console.error(error);
