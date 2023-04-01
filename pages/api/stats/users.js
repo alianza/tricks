2494cf61a70869af64ebd82dd2153d43258a2023
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-02 01:39:43 +0200

    Cleanup

--- pages/api/stats/users.js
+++ pages/api/stats/users.js
@@ -17,8 +17,6 @@ export default async function handler(req, res) {
         const manualUserIds = await Manual.distinct('userId');
         const comboUserIds = await Combo.distinct('userId');
 
-        console.log(`grindUserIds`, grindUserIds);
-
         const count = new Set([...grindUserIds, ...flatgroundTricksUserIds, ...manualUserIds, ...comboUserIds]).size;
 
         res.status(200).json({ success: true, data: { count } });
