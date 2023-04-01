b98116f9981691068b0f1b600cb9a40edbed91d6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-02 01:01:10 +0200

    Update server side utils

--- pages/api/utils.js
+++ pages/api/utils.js
@@ -1,27 +1,19 @@
 import Combo from '../../models/Combo';
 import { sOrNoS } from '../../lib/util';
 import { getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
 
-export const checkForUsedCombos = async ({ _id, trickType, res }) => {
+// Server Side Utils
+
+export const checkForUsedCombos = async (_id, trickType) => {
   const combos = await Combo.countDocuments({ 'trickArray.trick': _id });
 
-  if (combos) {
-    return res.status(400).json({
-      success: false,
-      error: `This ${trickType} is used in ${combos} combo${sOrNoS(combos)}`,
-    });
-  }
+  if (combos) throw new Error(`This ${trickType} is used in ${combos} combo${sOrNoS(combos)}`);
 };
 
-async function loginBarrier(context, authOptions) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
+export async function loginBarrier(req, res) {
+  let session = await getServerSession(req, res, authOptions);
+  if (!session) return res.status(401).json({ success: false, error: 'Unauthorized' });
+  const authQuery = { userId: session.user.id };
+  return { authQuery, session };
 }
