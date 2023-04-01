9872285f913100aceb4cab54f3d051a48d35de99
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-01 18:37:24 +0200

    Added avatars.githubusercontent.com images domain

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
 const prod = process.env.NODE_ENV === 'production';
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true',
 });
 
 const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: !prod,
-    // buildExcludes: [/middleware-manifest\.json$/]
+  dest: 'public',
+  disable: !prod,
+  // buildExcludes: [/middleware-manifest\.json$/]
 });
 
 const nextConfig = {
-
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
 };
 
-
 module.exports = withBundleAnalyzer(
-    withPWA({
-        ...nextConfig,
-    })
+  withPWA({
+    ...nextConfig,
+  })
 );
