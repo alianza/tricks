e9dc52b94481b26d8cd7d6c4d87c83d55a9a7286
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-02 01:00:23 +0200

    Added optional label to IconLink.jsx

--- components/common/IconLink.jsx
+++ components/common/IconLink.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 
-const IconLink = ({ href = '', onClick, Icon, title }) => {
+const IconLink = ({ href = '', onClick, Icon, title, label }) => {
   return (
-    <Link
-      className="block h-6 w-6 transition-transform duration-300 hover:scale-125 hover:duration-75 active:scale-95"
-      href={href}
-      inClick={onClick}
-      title={title ? title : 'New'}
-    >
-      <Icon className="h-6 w-6" />
+    <Link className="group flex h-6 hover:underline" href={href} onClick={onClick} title={title ? title : 'New'}>
+      {label && <span>{label}</span>}
+      <Icon className="h-6 w-6 transition-transform duration-300 active:scale-95  group-hover:scale-125 group-hover:duration-75" />
     </Link>
   );
 };
