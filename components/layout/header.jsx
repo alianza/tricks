a3ad3a5fb8b37627c5c1658c2595a5b1880b0d1a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-02 02:57:10 +0200

    Added google next-auth provider

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-10 flex h-20 w-full items-center justify-between bg-blue-600 p-4 text-right text-neutral-50 shadow-xl">
-      <Link href="/" className="h-[120%] shrink-0">
+      <Link href="/" className="block h-[120%] shrink-0">
         <img
           src="/logo.webp"
           alt="Skateboard Trick Tracker Logo"
