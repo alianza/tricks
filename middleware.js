e59e5f0adcb425634bbf0b748d468be410d3beec
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-10 02:23:23 +0200

    Added profile page to middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/dashboard', '/new-combo', '/new-flatground-trick', '/new-grind', '/new-manual'] }; // Must be authenticated to access these pages
+export const config = {
+  matcher: ['/dashboard', '/new-combo', '/new-flatground-trick', '/new-grind', '/new-manual', '/profile'],
+}; // Must be authenticated to access these pages
