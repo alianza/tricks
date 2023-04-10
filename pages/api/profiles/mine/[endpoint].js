30b8775b23d8629eb7ba587439403fa8242cdb48
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-10 03:23:40 +0200

    Move endpoints

--- pages/api/profiles/mine/[endpoint].js
+++ pages/api/profiles/mine/[endpoint].js
@@ -1,7 +1,6 @@
 import dbConnect from '../../../../lib/dbConnect';
-import { loginBarrier, notFoundHandler } from '../../../../lib/serverUtils';
+import { ensureProfile, loginBarrier } from '../../../../lib/serverUtils';
 import { authOptions } from '../../auth/[...nextauth]';
-import Profile from '../../../../models/Profile';
 
 export default async function handler(req, res) {
   const {
@@ -15,18 +14,9 @@ export default async function handler(req, res) {
   switch (method) {
     case 'GET':
       switch (endpoint) {
-        case '/':
-          try {
-            const profile = await Profile.findOneAndUpdate({ ...authQuery }, {}, { new: true, upsert: true });
-            res.status(200).json({ success: true, data: profile });
-          } catch (error) {
-            console.error(error);
-            res.status(400).json({ success: false, error: error.message });
-          }
-          break;
         case 'preferred_stance':
           try {
-            const profile = await Profile.findOneAndUpdate({ ...authQuery }, {}, { new: true, upsert: true });
+            const profile = await ensureProfile({ ...authQuery });
             res.status(200).json({ success: true, data: { preferred_stance: profile.preferred_stance } });
           } catch (error) {
             console.error(error);
@@ -36,19 +26,6 @@ export default async function handler(req, res) {
         default:
           res.status(400).json({ success: false, error: `Unhandled endpoint: ${endpoint}` });
           break;
-      }
-      break;
-    case 'PATCH':
-      switch (endpoint) {
-        case '/':
-          try {
-            const profile = await Profile.findOneAndUpdate({ ...authQuery }, req.body, { new: true });
-            if (!profile) return notFoundHandler(res, { label: 'Profile not found...' });
-            res.status(200).json({ success: true, data: profile });
-          } catch (error) {
-            console.error(error);
-            res.status(400).json({ success: false, error: error.message });
-          }
       }
       break;
     default:
