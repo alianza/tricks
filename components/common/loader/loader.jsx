1c9cd29d0d1ad844c103f50577aa0b242d5c0e7c
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-16 04:21:49 +0200

    Updated loader component

--- components/common/loader/loader.jsx
+++ components/common/loader/loader.jsx
@@ -1,5 +1,13 @@
-import styles from './loader.module.scss';
-
 export default function Loader({ className }) {
-  return <div className={`${styles.loader} before:bg-red-500 ${className}`} />;
+  return (
+    <svg
+      className={`${className} h-16 w-16 animate-spin text-white`}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2"></circle>
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+    </svg>
+  );
 }
