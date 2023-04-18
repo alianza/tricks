d374e653924465a07b97ee50bfd709b64db3b9aa
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-04-18 23:52:27 +0200

    Added statistics page to protected routes in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard', '/new-combo', '/new-flatground-trick', '/new-grind', '/new-manual', '/profile'],
+  matcher: ['/dashboard', '/new-combo', '/new-flatground-trick', '/new-grind', '/new-manual', '/profile', '/statistics'],
 }; // Must be authenticated to access these pages
