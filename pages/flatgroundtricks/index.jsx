65399e1d3d6f42d6bd5361f57a5fb8a5154406a4
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-05 17:23:31 +0200

    Refactored flatgroundtricks page to use GenericTable component

--- pages/flatgroundtricks/index.jsx
+++ pages/flatgroundtricks/index.jsx
@@ -4,11 +4,17 @@ import { useAsyncEffect } from '../../lib/customHooks';
 import { toast } from 'react-toastify';
 import Loader from '../../components/common/loader/loader';
 import { apiCall } from '../../lib/clientUtils';
+import Link from 'next/link';
+import utilStyles from '../../styles/utils.module.scss';
+import GenericTable from '../../components/common/genericTable/genericTable';
 
 export default function FlatgroundTricksPage() {
   const [flatgroundTricks, setFlatgroundTricks] = useState([]);
-  const flatgroundColumns = ['stance', 'direction', 'rotation', 'name', 'trick'];
-  const flatgroundActions = ['edit', 'view', 'delete'];
+  const flatgroundColumns = ['stance', 'direction', 'rotation', 'name', { trick: { className: 'text-sm font-bold' } }];
+  /* prettier-ignore */ const flatgroundActions = [
+    { edit: (obj) => <Link href={`/flatgroundtricks/${obj._id}/edit`} className={`${utilStyles.button} ${utilStyles.green}`}>Edit</Link> },
+    { view: (obj) => <Link href={`/flatgroundtricks/${obj._id}`} className={`${utilStyles.button} ${utilStyles.blue}`}>View</Link> },
+    { delete: () => <button className={`${utilStyles.button} ${utilStyles.red}`}>Delete</button> } ];
   const [loading, setLoading] = useState(true);
 
   useAsyncEffect(async () => {
@@ -22,6 +28,21 @@ export default function FlatgroundTricksPage() {
     }
   }, []);
 
+  const handleAction = async (action, obj) => {
+    switch (action) {
+      case 'delete':
+        try {
+          if (!confirm(`Are you sure you want to delete "${obj.trick}"?`)) return;
+          await apiCall('flatgroundtricks', { method: 'DELETE', id: obj._id });
+          const { data } = await apiCall('flatgroundtricks', { method: 'GET' });
+          setFlatgroundTricks(data);
+        } catch (error) {
+          toast.error(`Failed to delete ${obj.trick}: ${error.message}`);
+        }
+        break;
+    }
+  };
+
   return (
     <div className="flex flex-col gap-16">
       <div>
@@ -33,11 +54,12 @@ export default function FlatgroundTricksPage() {
       {loading ? (
         <Loader className="mx-auto my-24" />
       ) : (
-        <Table
+        <GenericTable
           objArray={flatgroundTricks}
           columns={flatgroundColumns}
           actions={flatgroundActions}
-          endpoint="flatgroundtricks"
+          onAction={handleAction}
+          entityName="flatground trick"
           newLink="/new-flatground-trick"
           showCount
         />
