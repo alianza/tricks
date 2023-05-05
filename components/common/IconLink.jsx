f65689b2e0b1bfde045a856763a685a6895894bd
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-05-05 17:15:04 +0200

    Added spread props to IconLink.jsx component

--- components/common/IconLink.jsx
+++ components/common/IconLink.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 import utilStyles from '../../styles/utils.module.scss';
 
-const IconLink = ({ href = '', onClick, Icon, title, label }) => {
+const IconLink = ({ href = '', onClick, Icon, title, label, ...props }) => {
   return (
-    <Link className={`${utilStyles.link} group flex`} href={href} onClick={onClick} title={title ? title : 'New'}>
+    <Link className={`${utilStyles.link} group flex`} href={href} onClick={onClick} title={title ? title : 'New'} {...props}>
       {label && <span>{label}</span>}
-      <Icon className="h-6 w-6 transition-transform duration-300 active:scale-95  group-hover:scale-125 group-hover:duration-75" />
+      <Icon className="h-6 w-6 transition-transform duration-300 active:scale-95 group-hover:scale-[120%] group-hover:duration-75" />
     </Link>
   );
 };
