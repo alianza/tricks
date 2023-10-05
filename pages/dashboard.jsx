bd6b2acd33a5f7df14096dad1470b228d040cac8
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2023-10-06 00:54:05 +0200

    Switch to all client side data fetching

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,56 +1,71 @@
-import dbConnect from '../lib/dbConnect';
-import { getCombos, getTricks } from '../lib/serverUtils';
-import { Model } from 'mongoose';
-import FlatGroundTrick from '../models/FlatgroundTrick';
-import Grind from '../models/Grind';
-import Combo from '../models/Combo';
-import Manual from '../models/Manual';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]';
 import LinkWithArrow from '../components/common/LinkWithArrow';
 import GenericTable from '../components/common/genericTable/genericTable';
 import { apiCall, getCommonActions, trickCol } from '../lib/clientUtils';
 import { toast } from 'react-toastify';
 import { useState } from 'react';
+import { useAsyncEffect } from '../lib/customHooks';
 
-export async function getServerSideProps(context) {
-  await dbConnect();
+// export async function getServerSideProps(context) {
+//   await dbConnect();
+//
+//   const session = await getServerSession(context.req, context.res, authOptions);
+//   const query = { userId: session.user.id };
+//
+//   // const flatgroundTricks = await getTricks(FlatGroundTrick, Model.find, query);
+//   const flatgroundColumns = ['stance', 'direction', 'rotation', 'name', trickCol];
+//
+//   // const grinds = await getTricks(Grind, Model.find, query);
+//   const grindColumns = ['stance', 'direction', 'name', trickCol];
+//
+//   // const manuals = await getTricks(Manual, Model.find, query);
+//   const manualColumns = [{ type: { className: 'text-sm font-bold' } }];
+//
+//   // let combos = await getCombos(Combo, Model.find, query);
+//   const comboColumns = [{ combo: { className: 'text-sm font-bold' } }];
+//
+//   return {
+//     props: {
+//       flatgroundTricksData: { flatgroundColumns },
+//       grindsData: { grindColumns },
+//       manualsData: { manualColumns },
+//       combosData: { comboColumns },
+//     },
+//   };
+// }
 
-  const session = await getServerSession(context.req, context.res, authOptions);
-  const query = { userId: session.user.id };
-
-  const flatgroundTricks = await getTricks(FlatGroundTrick, Model.find, query);
-  const flatgroundColumns = ['stance', 'direction', 'rotation', 'name', trickCol];
-
-  const grinds = await getTricks(Grind, Model.find, query);
-  const grindColumns = ['stance', 'direction', 'name', trickCol];
-
-  const manuals = await getTricks(Manual, Model.find, query);
-  const manualColumns = [{ type: { className: 'text-sm font-bold' } }];
-
-  let combos = await getCombos(Combo, Model.find, query);
-  const comboColumns = [{ combo: { className: 'text-sm font-bold' } }];
-
-  return {
-    props: {
-      flatgroundTricksData: { flatgroundTricks, flatgroundColumns },
-      grindsData: { grinds, grindColumns },
-      manualsData: { manuals, manualColumns },
-      combosData: { combos, comboColumns },
-    },
-  };
-}
-
-export default function Index({ flatgroundTricksData, grindsData, combosData, manualsData }) {
-  const [flatgroundTricks, setFlatgroundTricks] = useState(flatgroundTricksData.flatgroundTricks);
-  const [grinds, setGrinds] = useState(grindsData.grinds);
-  const [manuals, setManuals] = useState(manualsData.manuals);
-  const [combos, setCombos] = useState(combosData.combos);
+export default function Index({ flatgroundTricksData = {}, grindsData = {}, combosData = {}, manualsData = {} }) {
+  const [flatgroundTricks, setFlatgroundTricks] = useState(null);
+  const [grinds, setGrinds] = useState(null);
+  const [manuals, setManuals] = useState(null);
+  const [combos, setCombos] = useState(null);
 
   flatgroundTricksData.flatgroundActions = getCommonActions('flatgroundtricks');
   grindsData.grindActions = getCommonActions('grinds');
   manualsData.manualActions = getCommonActions('manuals');
   combosData.comboActions = getCommonActions('combos');
+
+  flatgroundTricksData.flatgroundColumns = ['stance', 'direction', 'rotation', 'name', trickCol];
+  grindsData.grindColumns = ['stance', 'direction', 'name', trickCol];
+  manualsData.manualColumns = [{ type: { className: 'text-sm font-bold' } }];
+  combosData.comboColumns = [{ combo: { className: 'text-sm font-bold' } }];
+
+  // Client-side fetch
+  useAsyncEffect(async () => {
+    try {
+      const trickTypes = ['flatgroundtricks', 'grinds', 'manuals', 'combos'];
+      const promises = trickTypes.map((type) => apiCall(type, { method: 'GET' }));
+      const [flatgroundTricks, grinds, manuals, combos] = await Promise.allSettled(promises);
+      setFlatgroundTricks(flatgroundTricks.value?.data);
+      setGrinds(grinds.value?.data);
+      setManuals(manuals.value?.data);
+      setCombos(combos.value?.data);
+      [flatgroundTricks, grinds, manuals, combos].forEach(({ status, reason }) => {
+        if (status === 'rejected') throw new Error(`Failed to fetch ${trickTypes.shift()} ${reason}`);
+      });
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }, []);
 
   const handleActions = async (action, obj, entityType) => {
     const endpointSetterMap = {
