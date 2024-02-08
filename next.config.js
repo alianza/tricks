a324deba01c8c578555532dba8922a85ddfd6df3
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-08 21:17:03 +0100

    Fix config

--- next.config.js
+++ next.config.js
@@ -11,20 +11,21 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'lh3.googleusercontent.com',
+    //     port: '',
+    //     pathname: '/**',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'avatars.githubusercontent.com',
+    //     port: '',
+    //     pathname: '/**',
+    //   },
+    // ],
   },
 };
 
