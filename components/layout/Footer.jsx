165d24ea10b36ed73adab978c86df50ded958f0e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-12 21:56:20 +0100

    Fix footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,15 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="h-footer w-full items-center bg-blue-600 px-6 text-neutral-50 shadow-xl">
-      <div className="flex h-full items-center justify-between">
-        <p>
-          Authored by:{' '}
-          <a className="underline" href="https://jwvbremen.nl/">
-            Jan-Willem van Bremen
-          </a>
-        </p>
-        <p>2023</p>
-      </div>
+    <footer className="min-h-footer flex justify-between w-full gap-4 items-center bg-blue-600 px-6 py-2 text-neutral-50 shadow-xl">
+      <p>
+        Authored by:{' '}
+        <a className="underline" href="https://jwvbremen.nl/">
+          Jan-Willem van Bremen
+        </a>
+      </p>
+      <p>2023</p>
     </footer>
   );
 }
