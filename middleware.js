5ab97c8030a51c08348cb8efb3c356c3566b370f
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-13 22:00:43 +0100

    Add routes to muddleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,17 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard', '/new-combo', '/new-flatground-trick', '/new-grind', '/new-manual', '/profile', '/statistics'],
+  matcher: [
+    '/dashboard',
+    '/new-combo',
+    '/new-flatground-trick',
+    '/new-grind',
+    '/new-manual',
+    '/profile',
+    '/statistics',
+    '/manuals/*',
+    '/grinds/*',
+    '/flatground-tricks/*',
+    '/combos/*',
+  ],
 }; // Must be authenticated to access these pages
