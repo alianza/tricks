1127fab68359eb65d9c001691a6c685bcc0625d0
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-20 18:56:28 +0100

    C;eanup

--- models/constants/trickTypes.js
+++ models/constants/trickTypes.js
@@ -9,13 +9,13 @@ const TRICK_TYPES_ENUM = Object.values(TRICK_TYPES);
 
 const DEFAULT_TRICK_TYPE = TRICK_TYPES.flatground;
 
-export const TRICK_TYPES_MODELS = {
+const TRICK_TYPES_MODELS = {
   [TRICK_TYPES.flatground]: 'FlatgroundTrick',
   [TRICK_TYPES.grind]: 'Grind',
   [TRICK_TYPES.manual]: 'Manual',
   [TRICK_TYPES.combo]: 'Combo',
 };
 
-export { TRICK_TYPES_ENUM, TRICK_TYPES, DEFAULT_TRICK_TYPE };
+export { TRICK_TYPES_ENUM, TRICK_TYPES, DEFAULT_TRICK_TYPE, TRICK_TYPES_MODELS };
 
 export default TRICK_TYPES;
