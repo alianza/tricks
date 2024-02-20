65439359006c65388a9fe33a4dfacd225cf6a6b5
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-20 16:37:43 +0100

    Cleanup

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="min-h-footer flex justify-between w-full gap-4 items-center bg-blue-600 px-6 py-2 text-neutral-50 shadow-xl">
+    <footer className="z-10 min-h-footer flex justify-between w-full gap-4 items-center bg-blue-600 px-6 py-2 text-neutral-50 shadow-xl">
       <p>
         Authored by:{' '}
         <a className="underline" href="https://jwvbremen.nl/">
--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -36,6 +36,7 @@ export default function Layout({ children }) {
           {children}
         </main>
       </div>
+
       <Footer />
     </div>
   );
