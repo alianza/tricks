16e8fa1513de9bb475970bf6f760cf7b50d17317
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-03-13 01:37:19 +0100

    Add year

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="z-10 min-h-footer flex justify-between w-full gap-4 items-center bg-blue-600 px-6 py-2 text-neutral-50 shadow-xl">
+    <footer className="z-10 flex min-h-footer w-full items-center justify-between gap-4 bg-blue-600 px-6 py-2 text-neutral-50 shadow-xl">
       <p>
         Authored by:{' '}
         <a className="underline" href="https://jwvbremen.nl/">
           Jan-Willem van Bremen
         </a>
       </p>
-      <p>2023</p>
+      <p>{`© ${new Date().getFullYear()}`}</p>
     </footer>
   );
 }
