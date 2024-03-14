9c1d4c5099200911b51d141b6d760a8912162f07
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-03-14 15:32:30 +0100

    Added support for bigspins flatground trick names

--- __tests__/lib/trickNames.test.js
+++ __tests__/lib/trickNames.test.js
@@ -1,14 +1,43 @@
 import { getFullTrickName } from '../../lib/commonUtils';
+import { FLATGROUND_TRICKS, DIRECTIONS } from '../../models/constants/flatgroundTricks';
+import STANCES from '../../models/constants/stances';
+
+const {
+  ollie,
+  shove_it,
+  kickflip,
+  heelflip,
+  varial_kickflip,
+  varial_heelflip,
+  hardflip,
+  inward_heelflip,
+  tre_flip,
+  lazer_flip,
+} = FLATGROUND_TRICKS;
+const { regular, fakie, switch: switchStance, nollie } = STANCES;
+const { none, frontside, backside } = DIRECTIONS;
 
 describe('Should resolve trick names correctly', () => {
   describe('Flatground Tricks', () => {
     it.each`
-      trick                                                                              | expected
-      ${{ stance: 'regular', direction: 'none', rotation: '0', name: 'ollie' }}          | ${'Ollie'}
-      ${{ stance: 'switch', direction: 'none', rotation: '0', name: 'ollie' }}           | ${'Switch Ollie'}
-      ${{ stance: 'regular', direction: 'none', rotation: '0', name: 'kickflip' }}       | ${'Kickflip'}
-      ${{ stance: 'fakie', direction: 'none', rotation: '0', name: 'kickflip' }}         | ${'Fakie Kickflip'}
-      ${{ stance: 'regular', direction: 'backside', rotation: '360', name: 'kickflip' }} | ${'Backside 360 Kickflip'}
+      trick                                                                                | expected
+      ${{ stance: regular, direction: none, rotation: '0', name: ollie }}                  | ${'Ollie'}
+      ${{ stance: switchStance, direction: none, rotation: '0', name: ollie }}             | ${'Switch Ollie'}
+      ${{ stance: regular, direction: none, rotation: '0', name: tre_flip }}               | ${'360 Flip'}
+      ${{ stance: nollie, direction: none, rotation: '0', name: inward_heelflip }}         | ${'Nollie Inward Heelflip'}
+      ${{ stance: regular, direction: none, rotation: '0', name: kickflip }}               | ${'Kickflip'}
+      ${{ stance: fakie, direction: none, rotation: '0', name: heelflip }}                 | ${'Fakie Heelflip'}
+      ${{ stance: regular, direction: backside, rotation: '360', name: kickflip }}         | ${'Backside 360 Kickflip'}
+      ${{ stance: regular, direction: backside, rotation: '180', name: shove_it }}         | ${'Bigspin'}
+      ${{ stance: regular, direction: backside, rotation: '180', name: shove_it }}         | ${'Bigspin'}
+      ${{ stance: regular, direction: frontside, rotation: '360', name: shove_it }}        | ${'Biggerspin'}
+      ${{ stance: regular, direction: backside, rotation: '180', name: varial_kickflip }}  | ${'Bigspin Kickflip'}
+      ${{ stance: regular, direction: backside, rotation: '360', name: varial_kickflip }}  | ${'Biggerflip'}
+      ${{ stance: regular, direction: frontside, rotation: '180', name: varial_heelflip }} | ${'Bigspin Heelflip'}
+      ${{ stance: regular, direction: frontside, rotation: '180', name: varial_heelflip }} | ${'Bigspin Heelflip'}
+      ${{ stance: nollie, direction: backside, rotation: '180', name: varial_heelflip }}   | ${'Nollie Bigspin Heelflip'}
+      ${{ stance: fakie, direction: backside, rotation: '180', name: varial_kickflip }}    | ${'Fakie Bigspin Kickflip'}
+      ${{ stance: nollie, direction: frontside, rotation: '180', name: varial_kickflip }}  | ${'Nollie Bigspin Kickflip'}
     `('should resolve trick name correctly: $expected', ({ trick, expected }) => {
       expect(getFullTrickName(trick)).toBe(expected);
     });
--- models/constants/flatgroundTricks.js
+++ models/constants/flatgroundTricks.js
@@ -7,12 +7,22 @@ const FLATGROUND_TRICKS = {
   varial_heelflip: 'varial heelflip',
   hardflip: 'hardflip',
   inward_heelflip: 'inward heelflip',
+  tre_flip: '360 flip',
+  lazer_flip: 'lazer flip',
 };
+
+const DIRECTIONS = {
+  none: 'none',
+  frontside: 'frontside',
+  backside: 'backside',
+};
+
+const DIRECTIONS_ENUM = Object.values(DIRECTIONS);
 
 const FLATGROUND_TRICKS_ENUM = Object.values(FLATGROUND_TRICKS);
 
 const DEFAULT_FLATGROUND_TRICK = FLATGROUND_TRICKS.ollie;
 
-export { FLATGROUND_TRICKS_ENUM, FLATGROUND_TRICKS, DEFAULT_FLATGROUND_TRICK };
+export { FLATGROUND_TRICKS_ENUM, FLATGROUND_TRICKS, DEFAULT_FLATGROUND_TRICK, DIRECTIONS, DIRECTIONS_ENUM };
 
 export default FLATGROUND_TRICKS;
