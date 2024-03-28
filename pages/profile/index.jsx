32b6fded4f55acefb7c7a65732b94da1151b16fb
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-03-28 12:36:37 +0100

    Cleanup

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,17 +1,13 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]';
-import Profile from '../../models/Profile';
-import findAndSerializeDoc from '../../lib/serverUtils';
-import { Model } from 'mongoose';
 import ProfileForm from '../../components/forms/ProfileForm';
+import { ensureProfile, serialize } from '../../lib/serverUtils';
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
   const query = { userId: session.user.id };
 
-  const profile = await findAndSerializeDoc(Profile, Model.findOneAndUpdate, query, {
-    args: [{}, { new: true, upsert: true }],
-  });
+  const profile = serialize(await ensureProfile({ ...query }));
 
   return {
     props: {
