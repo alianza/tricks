e1f0dbfdc837ac3a876c301f73107bd6a28583d7
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-04-25 01:29:26 +0200

    Revert "Add return"
    This reverts commit 79b3e540e5a1e8b88281b0624c6499308da9394a.

--- pages/api/flatgroundtricks/index.js
+++ pages/api/flatgroundtricks/index.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
           ...flatgroundTrick,
           trick: getFullTrickName(flatgroundTrick),
         }));
-        return res.status(200).json({ success: true, data });
+        res.status(200).json({ success: true, data });
       } catch (error) {
         console.error(error);
         res.status(400).json({ success: false, error: error.message });
