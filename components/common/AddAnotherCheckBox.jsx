397819579e1d20d097c5fe8c98f1c16fd9c5aebe
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-05-01 02:48:36 +0200

    Cleanup AddAnotherCheckBox.jsx

--- components/common/AddAnotherCheckBox.jsx
+++ components/common/AddAnotherCheckBox.jsx
@@ -1,6 +1,6 @@
 function AddAnotherCheckBox({ checked, onChange }) {
   return (
-    <label className="ml-1" title="Reset the form after creation">
+    <label title="Reset the form after creation">
       <input type="checkbox" checked={checked} onChange={onChange} name="addAnother" className="h-4 w-4 align-middle" />
       <span className="ml-2 align-middle">Add another</span>
     </label>
