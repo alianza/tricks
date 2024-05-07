9bd8f91e546f3dad1ecf3e63cb3b683bebc3875a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-05-08 00:41:36 +0200

    Added loader

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -3,6 +3,7 @@ import { authOptions } from '../api/auth/[...nextauth]';
 import ProfileForm from '../../components/forms/ProfileForm';
 import { ensureProfile, serialize } from '../../lib/serverUtils';
 import dbConnect from '../../lib/dbConnect';
+import Loader from '../../components/common/loader/loader';
 
 export async function getServerSideProps(context) {
   await dbConnect();
@@ -25,6 +26,8 @@ export async function getServerSideProps(context) {
 
 const ProfilePage = ({ profile }) => {
   console.log(`profile`, profile);
+
+  if (!profile) return <Loader className="mx-auto my-24" />;
 
   const profileForm = {
     preferred_stance: profile.preferred_stance,
