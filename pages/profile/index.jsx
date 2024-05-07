78f940e75f478689e8b3614113874d868d0ce8e6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-05-08 00:31:13 +0200

    log

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -20,6 +20,8 @@ export async function getServerSideProps(context) {
 }
 
 const ProfilePage = ({ profile }) => {
+  console.log(`profile`, profile);
+
   const profileForm = {
     preferred_stance: profile.preferred_stance,
   };
