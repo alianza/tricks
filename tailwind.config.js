6786377ac500b63e52323f0a1ad4345cdf32f039
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-05-08 02:14:01 +0200

    Added lib to tailwind.config.js content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './lib/**/*.{js,ts,jsx,tsx}'],
   plugins: [],
   theme: {
     extend: {
