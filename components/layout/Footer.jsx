eb5ce4b74281fffa905200ccd3a43bdbf1e30eb1
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-05-09 02:56:33 +0200

    Added slight shadow to Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="z-10 flex min-h-footer w-full items-center justify-between gap-4 bg-blue-600 px-6 py-2 text-neutral-50 shadow-xl">
+    <footer className="z-10 flex min-h-footer w-full items-center justify-between gap-4 bg-blue-600 px-6 py-2 text-neutral-50 shadow shadow-xl">
       <p>
         Authored by:{' '}
         <a className="underline" href="https://jwvbremen.nl/">
