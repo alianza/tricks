a350c7d437d1770e4da135fefeee0cead9570c41
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-05-20 21:22:56 +0200

    Refactor out underline hover effect as utility class

--- components/common/IconLink.jsx
+++ components/common/IconLink.jsx
@@ -4,8 +4,14 @@ import utilStyles from '../../styles/utils.module.scss';
 
 const IconLink = ({ href = '', onClick, Icon, title, label, ...props }) => {
   return (
-    <Link className={`${utilStyles.link} group flex`} href={href} onClick={onClick} title={title ? title : 'New'} {...props}>
-      {label && <span>{label}</span>}
+    <Link
+      className="underline-hover group flex items-center"
+      href={href}
+      onClick={onClick}
+      title={title ? title : 'New'}
+      {...props}
+    >
+      {label && <span className="text-nowrap">{label}</span>}
       <Icon className="h-6 w-6 transition-transform duration-300 active:scale-95 group-hover:scale-[120%] group-hover:duration-75" />
     </Link>
   );
--- components/common/LinkWithArrow.jsx
+++ components/common/LinkWithArrow.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
-import utilStyles from '../../styles/utils.module.scss';
 
 const LinkWithArrow = ({ label, href = '', onClick }) => {
   return (
     <Link
       href={href}
       onClick={onClick}
-      className={`${utilStyles.link} group relative mx-auto mb-6 flex items-center text-4xl`}
+      className="underline-hover group relative mx-auto mb-6 flex items-center text-4xl"
     >
       <h1 className="text-4xl">{label}</h1>
       <span className="absolute w-[calc(100%+2rem)] text-right text-3xl transition-[width] group-hover:w-[calc(100%+2.5rem)]">
