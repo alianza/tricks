0e9841d9d8dcaea0ace382ba9d40ec0a74abdb35
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-05-20 22:22:09 +0200

    Cleanup

--- pages/index.jsx
+++ pages/index.jsx
@@ -39,7 +39,7 @@ export default function Index() {
         {!session ? (
           <button
             onClick={() => signIn()}
-            className={`${utilStyles.button} w-full bg-gray-700 text-lg text-neutral-50  outline-gray-500 hover:bg-gray-800 focus:ring-gray-600/50 dark:bg-gray-300 dark:text-neutral-900 hover:dark:bg-gray-400`}
+            className={`${utilStyles.button} w-full bg-gray-700 text-lg text-neutral-50 outline-gray-500 hover:bg-gray-800 focus:ring-gray-600/50 dark:bg-gray-300 dark:text-neutral-900 hover:dark:bg-gray-400`}
           >
             Sign in to get started!
           </button>
